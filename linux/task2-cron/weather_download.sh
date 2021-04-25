8dd5d82a875aa6ea81ded3122531afcc4a4a57fb
ricci5791 <32428541+ricci5791@users.noreply.github.com>
2021-04-25 16:14:48 +0300

    fixed duplication of saving logic

--- linux/task2-cron/weather_download.sh
+++ linux/task2-cron/weather_download.sh
@@ -1,3 +1,3 @@
 #! usr/bin/bash
 
-cd $HOME && curl -o weather_forecast.json http://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=d370eeb23d0eb74a83a12f6b2596e0b5 > $HOME/weather.json
+cd $HOME && curl -o weather_forecast.json http://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=d370eeb23d0eb74a83a12f6b2596e0b5
