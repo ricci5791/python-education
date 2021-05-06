7ffc76933b7edde3fe5f158ac956349a6f8047f5
ricci5791 <htcone6759@gmail.com>
2021-05-06 19:28:28 +0300

    added parameters description for functions

--- python/task3/transports.py
+++ python/task3/transports.py
@@ -10,11 +10,13 @@ class MovableObject:
         self.velocity = 0.
 
     def move(self, new_coords: list[float, float]):
-        """Method which accepts list of 2 coords and set it to a instance"""
+        """Method which accepts list of 2 coords and set it to a instance
+        :param new_coords:"""
         self.coords = new_coords
 
     def change_speed(self, new_speed: float):
-        """Method which accepts float number(speed) and set it to a instance"""
+        """Method which accepts float number(speed) and set it to a instance
+        :param new_speed:"""
         self.velocity = new_speed
 
     def __str__(self):
@@ -37,18 +39,21 @@ class Transport(MovableObject):
         self.is_start_engine = True
 
     def move(self, new_coords: list[float, float]):
-        """Moves transport if engine ignited"""
+        """Moves transport if engine ignited
+        :param new_coords:"""
         if not self.is_start_engine:
             print("Firstly you need to ignite your engine!")
             return
         self.coords = new_coords
 
     def add_payload(self, payload_mass):
-        """Add payload to a transport"""
+        """Add payload to a transport
+        :param payload_mass:"""
         self.payload_mass = payload_mass
 
     def unload_payload(self, payload_mass):
-        """Unload payload from a transport. Can be unloaded fractionally"""
+        """Unload payload from a transport. Can be unloaded fractionally
+        :param payload_mass:"""
         if payload_mass > self.payload_mass:
             print(f"You can't unload {payload_mass}. It's too much!")
             return
@@ -61,7 +66,8 @@ class Transport(MovableObject):
 
 
 class Aircraft(Transport):
-    """Represents vehicle that can fly"""
+    """Represents vehicle that can fly
+    :param max_payload: Max carried weight"""
 
     def __init__(self, max_payload=5000):
         super().__init__()
@@ -73,13 +79,17 @@ class Aircraft(Transport):
                self.coords[1] - random.randint(1, 100)
 
     def add_payload(self, payload_mass):
-        """Add payload to a aircraft with some checks"""
+        """Add payload to a aircraft with some checks
+        :param payload_mass:"""
         if payload_mass > self.max_payload:
             print(f"Too much of payload. Max is {self.max_payload}")
 
 
 class Tanker(Transport):
-    """Represents ship that can carry petroleum"""
+    """Represents ship that can carry petroleum
+    :param name: Name of ship
+    :param max_route_length: Max path length without refuel
+    """
 
     def __init__(self, name, max_route_length=1000):
         super().__init__()
@@ -96,7 +106,9 @@ class Tanker(Transport):
 
 
 class Bus(Transport):
-    """Represents city bus with basic function to make stop"""
+    """Represents city bus with basic function to make stop
+    :param max_passengers: Max count of being boarded
+    """
 
     def __init__(self, max_passengers):
         super().__init__()
@@ -104,7 +116,8 @@ class Bus(Transport):
         self.passengers = 0
 
     def make_stop(self, passengers_diff):
-        """Add given passengers to a bus"""
+        """Add given passengers to a bus
+        :param passengers_diff:"""
         if self.passengers + passengers_diff > self.max_passengers:
             print("Too many passengers on board. Take less of them!")
             return
@@ -117,7 +130,10 @@ class Bus(Transport):
 
 class Subway(Transport):
     """Subway class that can append/pop wagons,
-     accepts wagon count and width of railway"""
+     accepts wagon count and width of railway
+     :param wagon_count:
+     :param railway_width:
+     """
 
     def __init__(self, wagon_count=5, railway_width=1460):
         super().__init__()
@@ -125,11 +141,13 @@ class Subway(Transport):
         self.wagon_count = wagon_count
 
     def append_wagons(self, wagon_count):
-        """Append wagon to a train"""
+        """Append wagon to a train
+        :param wagon_count:"""
         self.wagon_count += wagon_count
 
     def pop_wagons(self, wagon_count):
-        """Pop wagon from a train"""
+        """Pop wagon from a train
+        :param wagon_count:"""
         if wagon_count > self.wagon_count:
             print("Too many to pop!")
             return
@@ -138,3 +156,17 @@ class Subway(Transport):
     def get_railway_width(self):
         """Return standard railway path width"""
         return self.railway_width
+
+if __name__ == "__main__":
+    aircraft = Aircraft(2500)
+    subway = Subway(7)
+    bus = Bus(50)
+    tanker = Tanker("Susan")
+
+    print(aircraft.airport_distance())
+    aircraft.change_speed(550)
+
+    subway.pop_wagons(6)
+    subway.pop_wagons(6)
+
+# git commit -m "added parameters description for functions"
