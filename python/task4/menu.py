efb20699e24329ffd31eb47e5d28af2533d3a3f9
ricci5791 <htcone6759@gmail.com>
2021-05-17 21:04:19 +0300

    added Order functionality, created property of items to be ordered, can raise 'ValueError' if given item doesn't in the menu

---
+++ python/task4/menu.py
@@ -0,0 +1,10 @@
+"""Module with menu dictionary"""
+
+menu = {
+    "eggs": 15.99,
+    "potato": 4.99,
+    "tomato": 3.99,
+    "juice": 21.99,
+    "pork": 29.99,
+    "chicken": 25.99
+}
--- python/task4/orders.py
+++ python/task4/orders.py
@@ -3,21 +3,46 @@ import datetime
 import uuid
 from typing import List
 
+from menu import menu
+
 
 class Order:
     """Contains functions for orders representation functionality"""
+    menu = menu
 
     def __init__(self, item_list: List[str], customer: uuid):
         self.order_id = uuid.uuid4()
+        self._item_list = None
         self.item_list = item_list
         self.order_time = datetime.datetime.now()
-        self.price = self.calculate_price(item_list)
+        self.price = self.calculate_price()
         self.customer = customer
         self.status = "Received"
 
-    def calculate_price(self, item_list: List[str]) -> float:
+    def calculate_price(self) -> float:
         """Calculates price based on chosen items"""
-        return 0.0
+        price = 0.
+
+        for item in self.item_list:
+            price += self.menu.get(item)
+
+        return price
+
+    @property
+    def item_list(self):
+        """
+        Property value of order price
+        :return: Total price of a order
+        """
+        return self._item_list
+
+    @item_list.setter
+    def item_list(self, value: List[str]):
+        for item in value:
+            if item not in self.menu.keys():
+                raise ValueError(f"Item doesn't exist: {item}")
+
+        self._item_list = value
 
     def checkout(self) -> None:
         """Checkout order"""
