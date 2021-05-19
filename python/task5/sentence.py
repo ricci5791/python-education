1434a74716e03fbeea120e962f0bff14059df741
ricci5791 <htcone6759@gmail.com>
2021-05-19 11:07:24 +0300

    fixed unnecessary double underscore naming in variables and methods

--- python/task5/sentence.py
+++ python/task5/sentence.py
@@ -38,34 +38,54 @@ class Sentence:
     other_chars_pattern = re.compile(r_not_alpha)
 
     def __init__(self, sentence: str):
-        self.__check_type(sentence)
-        self.__check_punctuation(sentence)
+        self.check_type(sentence)
+        self.check_punctuation(sentence)
 
-        self.__sentence = sentence
-        self.chars_count = self.__set_chars_count()
-        self.other_count = len(self.__sentence) - self.chars_count
+        self._sentence = sentence
+        self.chars_count = self.set_chars_count()
+        self.other_count = len(self._sentence) - self.chars_count
 
-    def __check_type(self, sentence):
+    def check_type(self, sentence):
+        """
+        Checks if given sentence is string
+        :param sentence: Given sentence to test
+        :return: None
+        """
         if not isinstance(sentence, str):
             raise TypeError(f"Was given {type(sentence)}, str required instead")
 
-    def __check_punctuation(self, sentence):
+    def check_punctuation(self, sentence):
+        """
+        Checks if given sentence ended with punctuation chars
+        :param sentence: Given sentence to test
+        :return: None
+        """
         if sentence[-1] not in self.punctuation:
             raise ValueError(f"Sentence should be completed with '.!?',"
                              f" got {sentence[-1]} instead")
 
-    def __set_chars_count(self) -> int:
+    def set_chars_count(self) -> int:
+        """
+        Counts latin chars in sentence
+        :return: Number of latin chars
+        :rtype: int
+        """
         words_count = 0
-        for char in self.__sentence:
+        for char in self._sentence:
             if str.isalpha(char):
                 words_count += 1
         return words_count
 
-    def __del_other_chars(self) -> str:
-        return self.punctuation_pattern.sub("", self.__sentence)
+    def del_other_chars(self) -> str:
+        """
+        Delete from sentence punctuation chars
+        :return: Formatted string
+        :rtype: str
+        """
+        return self.punctuation_pattern.sub("", self._sentence)
 
     def _words(self):
-        for word in self.__sentence.split():
+        for word in self._sentence.split():
             yield self.punctuation_pattern.sub("", word)
 
     @property
@@ -75,7 +95,7 @@ class Sentence:
         :return: List of words
         :rtype: List[str]
         """
-        return self.punctuation_pattern.sub("", self.__sentence).split()
+        return self.punctuation_pattern.sub("", self._sentence).split()
 
     @property
     def other_chars(self) -> List[str]:
@@ -85,7 +105,7 @@ class Sentence:
         :return: Non-latin chars in the sentence
         :rtype: List[str]
         """
-        return self.other_chars_pattern.findall(self.__sentence)
+        return self.other_chars_pattern.findall(self._sentence)
 
     def __repr__(self):
         return f"<Sentence(words={self.chars_count}, " \
@@ -94,10 +114,10 @@ class Sentence:
     def __getitem__(self, item):
         """Delegates basic behavior of list.__getitem__ logic"""
         if isinstance(item, int):
-            return self.__del_other_chars().split()[item]
+            return self.del_other_chars().split()[item]
 
         if isinstance(item, slice):
-            return self.__del_other_chars().split()[item]
+            return self.del_other_chars().split()[item]
 
     def __iter__(self):
         """Returns SentenceIterator instance"""
