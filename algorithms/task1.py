7c4d317c1939d38ae6d238ac9ea985978ff92bd1
ricci5791 <htcone6759@gmail.com>
2021-05-31 17:57:36 +0300

    added factorial implementation

--- algorithms/task1.py
+++ algorithms/task1.py
@@ -24,3 +24,16 @@ def binary_search(arr: List[int], value: int) -> int:
             return pivot
 
     return -1
+
+
+def factorial(num: int) -> int:
+    """
+    Calculates factorial with recursive approach
+
+    :param num: Number to be factorialized
+    :return: Factorial of the given number
+    :rtype: int
+    """
+    if num == 0:
+        return 1
+    return num * factorial(num - 1)
