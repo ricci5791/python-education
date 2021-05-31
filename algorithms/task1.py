50ef94d174e28e34d81d25a24d1eac1260f70ca6
ricci5791 <htcone6759@gmail.com>
2021-05-31 20:23:02 +0300

    added quick sort implementation and tests for module

--- algorithms/task1.py
+++ algorithms/task1.py
@@ -1,5 +1,6 @@
 """Module with binary search, quick sort, factorial function"""
-from typing import List
+import math
+from typing import List, Tuple
 
 
 def binary_search(arr: List[int], value: int) -> int:
@@ -37,3 +38,55 @@ def factorial(num: int) -> int:
     if num == 0:
         return 1
     return num * factorial(num - 1)
+
+
+def partition(arr, left, right) -> int:
+    """
+    Swaps elements in the array by comparison with pivot element
+
+    :param arr: Array to be 'sorted'
+    :param left: left index of partition
+    :param right: right index of partition
+    """
+    middle_element = arr[(left + right) // 2]
+    temp_left = left
+    temp_right = right
+
+    while temp_left <= temp_right:
+        while arr[temp_left] < middle_element:
+            temp_left += 1
+        while arr[temp_right] > middle_element:
+            temp_right -= 1
+        if temp_left >= temp_right:
+            break
+        else:
+            arr[temp_left], arr[temp_right] = arr[temp_right], arr[temp_left]
+            temp_left += 1
+            temp_right -= 1
+    return temp_right
+
+
+def quick_sort(arr: List) -> List:
+    """
+
+
+    :param arr:
+    :return:
+    """
+    stack: List[Tuple] = list()
+    stack.append((0, len(arr) - 1))
+
+    while len(stack) != 0:
+        left, right = stack.pop()
+        if right <= left:
+            continue
+        index = partition(arr, left, right)
+
+        if index - left > right - index:
+            stack.append((left, index))
+            stack.append((index + 1, right))
+        else:
+            stack.append((index + 1, right))
+            stack.append((left, index))
+
+    return arr
