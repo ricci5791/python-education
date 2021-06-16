29741ac41bdc141b03aa91918d0294058f7ceb48
ricci5791 <htcone6759@gmail.com>
2021-06-16 12:55:33 +0300

    fixed wrong GROUP BY clause, syntax

--- databases/practice2-task3/task4/select_tops.sql
+++ databases/practice2-task3/task4/select_tops.sql
@@ -12,13 +12,13 @@ SELECT *
 FROM products
          FULL JOIN cart_product cp
                    ON products.product_id = cp.products_product_id
-         FULL JOIN carts c on cp.carts_cart_id = c.cart_id
-         FULL JOIN "Order" o on c.cart_id = o.carts_cart_id
+         FULL JOIN carts c ON cp.carts_cart_id = c.cart_id
+         FULL JOIN "Order" o ON c.cart_id = o.carts_cart_id
 WHERE o."order_id" IS NULL;
 
 -- 3. Вывести топ 10 продуктов, которые добавляли в корзины чаще всего.
 
-SELECT product_id, count(carts_cart_id) as product_count
+SELECT product_id, count(carts_cart_id) AS product_count
 FROM products p
          INNER JOIN cart_product cp ON p.product_id = cp.products_product_id
 GROUP BY p.product_id
@@ -28,12 +28,12 @@ LIMIT 10;
 -- 4. Вывести топ 10 продуктов, которые не только добавляли в корзины, но и оформляли заказы чаще всего.
 
 SELECT product_id,
-       count(o.order_id)       as order_count,
-       count(cp.carts_cart_id) as product_count
+       count(o.order_id)       AS order_count,
+       count(cp.carts_cart_id) AS product_count
 FROM products p
          INNER JOIN cart_product cp ON p.product_id = cp.products_product_id
-         INNER JOIN carts c on cp.carts_cart_id = c.cart_id
-         INNER JOIN "Order" o on c.cart_id = o.carts_cart_id
+         INNER JOIN carts c ON cp.carts_cart_id = c.cart_id
+         INNER JOIN "Order" o ON c.cart_id = o.carts_cart_id
 GROUP BY p.product_id
 ORDER BY order_count DESC
 LIMIT 10;
@@ -50,21 +50,21 @@ LIMIT 5;
 
 -- 6. Вывести топ 5 юзеров, которые сделали больше всего заказов (кол-во заказов).
 
-SELECT *, COUNT(o.order_id) order_count
+SELECT users.*, count(c.cart_id) order_count
 FROM users
-         INNER JOIN carts c on users.user_id = c.users_user_id
-         INNER JOIN "Order" o on c.cart_id = o.carts_cart_id
-GROUP BY users.user_id
+         JOIN carts c ON users.user_id = c.users_user_id
+         JOIN "Order" o ON c.cart_id = o.carts_cart_id
+GROUP BY user_id
 ORDER BY order_count DESC
 LIMIT 5;
 
 -- 7. Вывести топ 5 юзеров, которые создали корзины, но так и не сделали заказы.
 
-SELECT *, count(C.cart_id) AS cart_count
+SELECT users.*, count(c.cart_id) AS cart_count
 FROM users
-         INNER JOIN carts c on users.user_id = c.users_user_id
-         FULL JOIN "Order" o on c.cart_id = o.carts_cart_id
-WHERE O.order_id IS NULL
+         INNER JOIN carts c ON users.user_id = c.users_user_id
+         FULL JOIN "Order" o ON c.cart_id = o.carts_cart_id
+WHERE o.order_id IS NULL
 GROUP BY users.user_id
 ORDER BY cart_count DESC
 LIMIT 5;
