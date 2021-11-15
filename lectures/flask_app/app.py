ebc79c4e1a8017c3c140dd9910c942c66a219055
vadym <htcone6759@gmail.com>
2021-11-16 00:03:38 +0200

    added minio s3

--- lectures/flask_app/app.py
+++ lectures/flask_app/app.py
@@ -1,3 +1,10 @@
+import logging
+import os
+
+import requests
+import io
+
+from minio import Minio
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,5 +15,36 @@ def read_root():
     return jsonify('hello world')
 
 
-if __name__ == '__main__':
-    app.run()
+def _save_to_bucket():
+    minio_access_key = os.getenv("MINIO_ACCESS_KEY", '')
+    minio_secret_key = os.getenv("MINIO_SECRET_KEY", '')
+
+    client = Minio(
+        "s3:9000",
+        access_key=minio_access_key,
+        secret_key=minio_secret_key,
+        secure=False,
+    )
+
+    response = requests.get('https://poloniex.com/public?command=returnTradeHistory&currencyPair=USDT_BTC')
+    raw_json = str(response.json()).encode('utf-8')
+    bytes_json = io.BytesIO(raw_json)
+
+    if client.bucket_exists('testbucket'):
+        logging.info('bucket exists')
+    else:
+        logging.warning('bucket doesn\'t exist, create new one')
+        client.make_bucket('testbucket')
+
+    client.put_object('testbucket',
+                      'data.json',
+                      data=bytes_json,
+                      length=bytes_json.getbuffer().nbytes,
+                      content_type='application/json')
+
+    return '200'
+
+
+@app.get("/crypto")
+def get_crypto():
+    return _save_to_bucket()
